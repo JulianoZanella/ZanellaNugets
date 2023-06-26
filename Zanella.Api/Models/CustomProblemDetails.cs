239ea7e86c50a3409d64ce62019d58ff37cf4842
Juliano Zanella <juliano.zanella.live@gmail.com>
2023-06-26 12:18:18 -0300

    Add model state validation filter

---
+++ Zanella.Api/Models/CustomProblemDetails.cs
@@ -0,0 +1,29 @@
+﻿namespace Zanella.Api.Models
+{
+    /// <summary>
+    /// Errors in validation
+    /// </summary>
+    public class CustomProblemDetails : IProblemDetails
+    {
+        /// <summary>
+        /// List of Errors
+        /// </summary>
+        public IEnumerable<IProblemDetail> Errors { get; set; } = new List<ErrorField>();
+    }
+
+    /// <summary>
+    /// Errors in validation
+    /// </summary>
+    public class ErrorField : IProblemDetail
+    {
+        /// <summary>
+        /// Name of property
+        /// </summary>
+        public string Field { get; set; } = string.Empty;
+
+        /// <summary>
+        /// List of error messages
+        /// </summary>
+        public IEnumerable<string> Errors { get; set; } = new List<string>();
+    }
+}
